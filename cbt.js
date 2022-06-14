fa918630ee13c3977e464c8f677b07b8dd01add7
Ambrose Junior <adepojuifeoluwa999@gmail.com>
2022-06-14 11:35:45 +0100

    first commit

---
+++ cbt.js
@@ -0,0 +1,5 @@
+let option1 = document.querySelector('option1');
+let option2 = document.querySelector('option2');
+let option3 = document.querySelector('option3');
+let option4 = document.querySelector('option4');
+let question = document.querySelector('question')
